aff5d45f6dc483bb9c6b2d76956f4e9cb2d69524
Pedro Torchio <pedrotorchio@gmail.com>
2022-09-29 10:50:39 +1000

    refactoring and simplifying code base

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,28 @@
-import React from "react";
+import OneSDK, { SdkMode } from "@frankieone/one-sdk";
+import React, { FC, useEffect } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+
+const App: FC = () => {
+  useEffect(() => void useOneSDK());
+  return <div>Customer UI</div>;
+};
+
+async function useOneSDK() {
+  const oneSdk = await OneSDK({ mode: SdkMode.DUMMY });
+  const ocr = oneSdk.component("ocr");
+  ocr.start();
+  ocr.on("input_required", ocrInputRequiredCallback);
+  ocr.off("input_required", ocrInputRequiredCallback)
+}
+
+function ocrInputRequiredCallback(info, error, provideFile) {
+  alert(`
+    input_required
+    ${JSON.stringify(info, null, 2)}
+    ${JSON.stringify(error, null, 2)}`
+  );
+}
+
 
 createRoot(
   document.getElementById("root")!
